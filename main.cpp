983bcd7d44c7fd93219f5433929e8af65ae41019
AKHMAD SAIFUL ANAM <45529658+akhmadaan@users.noreply.github.com>
2018-12-09 00:50:31 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct pecahan
+{
+    int pb;
+    int py;    };
+    int main(int argc, char *argv[])
+
+    {
+        pecahan P,Q;
+        cout<<"Program mengubah pecahan ke bilangan bulat"<<endl;
+        cout<<"masukan Pembilang :";
+        cin>>P.pb;
+
+        cout<<"Masukan Penyebut :";
+        cin>>Q.py;
+
+        cout<<"\t   "<<(P.pb)<<endl;
+        cout<<"\tP= -"<<endl;
+        cout<<"\t    "<<(Q.py)<<endl;
+
+        if("/")
+        {
+            cout<<"Hasilnya ="<<(P.pb)/(Q.py)<<endl;
+        }
+
+    }
+
+
+
